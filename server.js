645e4cb7d60483db43d8aca01a9b0474d30ae3be
Adam <myemail>
2020-06-10 02:39:54 -0400

    Added player connect/disconnect logic

--- server.js
+++ server.js
@@ -28,27 +28,86 @@ server.listen(port, function() {
 
 
 //storage stuff
-let players = {}; //holds Player objects
+let players = {}; //holds Player objects, keys are player names (not socket ids)
+let id_to_name = {}; //maps socket ids to names. If a name isn't in here, player is disconnected
 
+class Player {
+	constructor(name){
+		this.name = name;
+		this.connected = true;
+		
+		this.ships = [new Ship(), new Ship()];
+		this.food = 0;
+		this.wood = 0;
+		this.bricks = 0;
+		this.money = 0;
+		
+		this.right_whales = 0;
+		this.bowhead_whales = 0;
+		this.sperm_whales = 0;
+	}
+}
 
-
+class Ship {
+	constructor(){
+		this.prepared = false; //if prepared and on the dock
+		this.distance = 0; //how far out to sea it is
+		this.right_whales = 0;
+		this.bowhead_whales = 0;
+		this.sperm_whales = 0;
+	}
+}
 
 
 // Add the WebSocket handlers
 io.on("connection", function(socket) {
 	
-	//when a new player joins, create a player object with the correct data
-	socket.on("new player", function(name){
-		players[socket.id] = name; //temp
-		console.log("New player: " + name + " (id: " + socket.id + ")");
+	//when a new player joins, check if player exists. If they don't, create new player. If they do, only allow join if that player was disconnected
+	socket.on("new player", function(name, callback){
+		if(!players.hasOwnProperty(name)){
+			//new player
+			players[name] = new Player(name);
+			id_to_name[socket.id] = name;
+			console.log("New player: " + name + " (id: " + socket.id + ")");
+			callback(true); //successful
+		}
+		else if(players[name].connected){
+			console.log(name + " is a duplicate name - asking them to try another");
+			callback(false); //duplicate name, tell the client it's invalid
+		}
+		else {
+			console.log(name + " reconnected (id: " + socket.id + ")");
+			id_to_name[socket.id] = name; //add the new mapping
+			callback(true); //successful
+		}
 	});
 	
 	//remove player when they leave
 	socket.on("disconnect", function(){
-		if(players[socket.id]){
-			console.log("Player disconnected (id: " + socket.id + ")");
-			delete players[socket.id];
+		if(id_to_name.hasOwnProperty(socket.id)){
+			player = players[id_to_name[socket.id]];
+			player.connected = false;
+			delete id_to_name[socket.id];
+			console.log(player.name+" disconnected (id: " + socket.id + ")");
 		}
 	});
 	
+	
+	socket.on("place_worker", function(place){ //place is a string
+		
+	});
+	
+	socket.on("choose_whale", function(whale, ship){ //whale: int, ship: 0|1
+		
+	});
 });
+
+
+
+/*
+Notes
+Track players by name, not socket id, so if disconnect then reconnect can associate with same player
+
+
+
+*/
--- static/client.js
+++ static/client.js
@@ -1,16 +1,29 @@
 //SETUP ---------------------------------------------------
 
-let name = prompt("Please enter a name:");
-if(!name || name===""){name = "unnamed";}
-
 let socket = io();
 let id; //id of the socket
 
 
 //CONNECTION TO SERVER -----------------------------------
 
-//send a new player message to the server
-socket.emit("new player", name);
+//send a new player message to the server, and pick name
+function registerName(){
+	let name = prompt("Please enter a name:"); //TODO: make this a GUI thing not a prompt
+	if(!name || name===""){
+		registerName();
+		return;
+	}
+	
+	socket.emit("new player", name, function(success){
+		console.log("success",success);
+		if(!success){
+			alert("'"+name+"' is taken. Please choose another");
+			registerName();
+		}
+	});
+}
+
+registerName();
 
 //store the id of the connection
 socket.on("connect", function(){
